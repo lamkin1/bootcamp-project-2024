4f3f767ef4f8e32339e80fefe370ce6b8887c8b9
lamkin1 <jamesclamkin@gmail.com>
2024-12-22 14:53:12 -0700

    more contact page updates

--- bootcamp-milestone2/src/app/about/page.tsx
+++ bootcamp-milestone2/src/app/about/page.tsx
@@ -5,15 +5,15 @@ export default function About() {
     return(
         <div>
             <h1 className='page-title'>Contact Me</h1>
-        <div className={style['contact-form']}>
-            <form id="contact-form">
+        <div className={style['contact-page']}>
+            <form className={style['contact-form']}>
                 <label className={style['contact-label']} htmlFor="name">Name: </label>
                 <input className={style['contact-text']} type="text" id="name" name="name" required/>
                 <label className={style['contact-label']}  htmlFor="email">Email: </label>
                 <input className={style['contact-text']} type="text" id="email" required/>
                 <label className={style['contact-label']}  htmlFor="message">Message: </label>
                 <textarea className={style['contact-text']} id="message" name="message" required></textarea>
-                <input id="contact-submit-btn" type="submit"/>
+                <input className={style['submit-button']} type="submit"/>
             </form>
         </div>
         </div>

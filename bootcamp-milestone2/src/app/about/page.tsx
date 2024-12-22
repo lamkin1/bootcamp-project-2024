ec0dfcf3c8c6763690dac43345332f40ce564c54
lamkin1 <jamesclamkin@gmail.com>
2024-12-22 14:37:44 -0700

    added conctact me section

--- bootcamp-milestone2/src/app/about/page.tsx
+++ bootcamp-milestone2/src/app/about/page.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import style from "./page.module.css";
 
 export default function About() {
     return(
         <div>
-            <p>Hi</p>
+            <h1 className='page-title'>Contact Me</h1>
+        <div className={style['contact-form']}>
+            <form id="contact-form">
+                <label className={style['contact-label']} htmlFor="name">Name: </label>
+                <input className={style['contact-text']} type="text" id="name" name="name" required/>
+                <label className={style['contact-label']}  htmlFor="email">Email: </label>
+                <input className={style['contact-text']} type="text" id="email" required/>
+                <label className={style['contact-label']}  htmlFor="message">Message: </label>
+                <textarea className={style['contact-text']} id="message" name="message" required></textarea>
+                <input id="contact-submit-btn" type="submit"/>
+            </form>
+        </div>
         </div>
     )
 }
